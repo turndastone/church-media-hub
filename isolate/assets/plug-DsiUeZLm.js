a95ddb70652aad1dc66c03813c211f3c2cd2df52
turndastone <215173164+turndastone@users.noreply.github.com>
2026-08-30 00:30:02 +0000

    Before: Remove provincial programs from the website and also anything else related to obs studio or church m...

--- isolate/assets/plug-DsiUeZLm.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-49UzBdOt.js";const a=[["path",{d:"M12 22v-5",key:"1ega77"}],["path",{d:"M9 8V2",key:"14iosj"}],["path",{d:"M15 8V2",key:"18g5xt"}],["path",{d:"M18 8v5a4 4 0 0 1-4 4h-4a4 4 0 0 1-4-4V8Z",key:"osxo6l"}]],t=o("plug",a);export{t as P};
